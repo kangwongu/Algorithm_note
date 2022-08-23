af5085ea8ac8ca6637cd60b210d6d209b487e4c8
kangwongu <kwg0527@naver.com>
2022-08-23 11:15:35 +0900

    4673 셀프넘버

--- baekjoon/4673.py
+++ baekjoon/4673.py
@@ -1,6 +1,10 @@
+self_number_list = list(map(int, range(1, 10001)))
 
-def self_number(n):
-    current_num = n
+for i in range(1, 10001):
+    N = (i) + (i//1000) + (i % 1000 // 100) + (i % 100 // 10) + (i%10)
 
+    if N in self_number_list:
+        self_number_list.remove(N)
 
-
+for self_number in self_number_list:
+    print(self_number)
